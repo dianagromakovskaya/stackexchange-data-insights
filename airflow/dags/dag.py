99d67a090fc9f6aa74b392f806133ca07cad42e9
dianagromakovskaya <dianagr1996@gmail.com>
2024-04-15 01:12:42 +0300

    added config

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -1,27 +1,33 @@
-#dags/example_hello_world.py
-"""Example dag for development. We create a dag with two tasks hello_world_1
-and hello_world_2 we then trigger 1 and then 2"""
-
 import datetime
+import json
 import logging
 import os
 import re
 import xml.etree.ElementTree as  ET
 
 import pandas as pd
+import yaml
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator, BigQueryCreateEmptyDatasetOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator, \
+    BigQueryDeleteTableOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-from google.cloud import storage
 from airflow.utils.task_group import TaskGroup
+from google.cloud import storage
 
 
-init_url = 'https://archive.org/download/stackexchange/'
-xml_file = 'Posts.xml'
-BUCKET = 'dtc-de-course-412710_stackexchange-data' # TODO: read from config file
+with open('config/config.yaml', 'r') as cfg:
+    CONFIG = yaml.safe_load(cfg)
+INIT_URL = CONFIG['init_url']
+BUCKET = f"{CONFIG['gcp']['project_id']}_{CONFIG['gcp']['bucket']}"
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+GCP_PROJECT = CONFIG['gcp']['project_id']
+SERVICES = CONFIG['services']
+DATASET = CONFIG['gcp']['dataset']
+START_DATE = CONFIG['start_date']
+
+
 logging.basicConfig(level=logging.INFO)
 
 
@@ -46,16 +52,11 @@ def xml_to_csv(file):
     df = pd.DataFrame.from_records(rows)
     df.rename(columns=lambda x: re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', x).lower(), inplace=True)
     csv_file = file.replace('xml', 'csv')
-    # df = df[['id', 'post_type_id', 'creation_date', 'score', 'view_count', 'body',
-    #    'owner_user_id', 'last_activity_date', 'title', 'tags', 'answer_count',
-    #    'comment_count', 'closed_date', 'content_license', 'accepted_answer_id',
-    #    'last_editor_user_id', 'last_edit_date', 'parent_id',
-    #    'owner_display_name', 'community_owned_date',
-    #    'last_editor_display_name', 'favorite_count']]
-    # df['body'] = df['body'].apply(lambda x: x.replace("\r", " "))
-    # df['body'] = df['body'].apply(lambda x: x.replace("\n", " "))
+    df['id'] = df['id'].fillna(0)
     df.to_csv(csv_file, index=False)
-    logging.info(df.columns)
+    logging.info(os.listdir('data'))
+    logging.info(os.listdir('.google'))
+    logging.info(os.listdir('.google/credentials'))
     return csv_file
 
 
@@ -77,134 +78,71 @@ def upload_to_gcs(bucket, object_name, local_file):
     blob.upload_from_filename(local_file)
 
 
-with DAG(
-    dag_id='upload_stackexchange_data_to_gcs',
-    start_date=datetime.datetime(2024, 4, 10),
-    tags=['stackexchange-data-insights'],
-    schedule_interval='@daily'
-) as dag:
-    files = ['Posts', 'Tags', 'Badges']
-    start = BashOperator(
-        task_id='start',
-        bash_command=f'echo HELLO'
-    )
-    for f in files:
-        with TaskGroup(f) as tg:
-            download_file = BashOperator(
-                task_id=f'download_{f}',
-                bash_command=f'curl -sSLf {init_url}datascience.stackexchange.com.7z/{f}.xml > {AIRFLOW_HOME}/{f}.xml'
-            )
-            convert_xml_to_csv = PythonOperator(
-                task_id=f'xml_to_csv_{f}',
-                python_callable=xml_to_csv,
-                op_kwargs=dict(file=f'{AIRFLOW_HOME}/{f}.xml')
-            )
-            upload_data_to_gcs = PythonOperator(
-                task_id=f'data_to_gcs',
-                python_callable=upload_to_gcs,
-                op_kwargs=dict(bucket=BUCKET,
-                               object_name=f'datascience/{f}.csv',
-                               local_file=f'{AIRFLOW_HOME}/{f}.csv')
-            )
-            download_file >> convert_xml_to_csv >> upload_data_to_gcs
-    start >> tg
+def get_table_schema(table):
+    with open(f'config/table_schemas/{table}.json', 'r') as file:
+        table_schema = json.load(file)['schema']
+    return table_schema
 
-    # download_file = BashOperator(
-    #     task_id='download_posts',
-    #     bash_command=f'curl -sSLf {init_url}datascience.stackexchange.com.7z/{xml_file} > {AIRFLOW_HOME}/{xml_file}'
-    # )
-    # convert_xml_to_csv = PythonOperator(
-    #     task_id=f'xml_to_csv',
-    #     python_callable=xml_to_csv,
-    #     op_kwargs=dict(file=f'{AIRFLOW_HOME}/{xml_file}')
-    # )
-    # upload_data_to_gcs = PythonOperator(
-    #     task_id=f'data_to_gcs',
-    #     python_callable=upload_to_gcs,
-    #     op_kwargs=dict(bucket=BUCKET,
-    #                    object_name='datascience/tags.csv',
-    #                    local_file=f'{AIRFLOW_HOME}/tags.csv')
-    # )
-    #
-    # load_csv = GCSToBigQueryOperator(
-    #     task_id='gcs_to_bigquery_example',
-    #     bucket=BUCKET,
-    #     source_objects=['datascience/tags.csv'],
-    #     destination_project_dataset_table=f"datascience_stackexchange.tags",
-    #     write_disposition='WRITE_TRUNCATE'
-    # )
+for SERVICE in SERVICES:
+    with DAG(
+        dag_id=f'upload_{SERVICE}_stackexchange_data_to_gcs',
+        # start_date=datetime.datetime.strptime(START_DATE, "%Y-%m-%d"),
+        start_date=START_DATE,
+        tags=['stackexchange-data-insights'],
+        schedule_interval='@daily'
+    ) as dag:
+        files = ['badges', 'comments', 'post_history', 'post_links', 'posts', 'tags', 'users', 'votes']
+        prev_group = None
+        for f in files:
+            with TaskGroup(f) as tg:
+                download_file = BashOperator(
+                    task_id=f'download_{f}',
+                    bash_command=f'curl -sSLf {INIT_URL}{SERVICE}.stackexchange.com.7z/{"".join([w.title() for w in f.split("_")])}.xml > {AIRFLOW_HOME}/data/{SERVICE}_{f}.xml'
+                )
+                convert_xml_to_csv = PythonOperator(
+                    task_id=f'xml_to_csv_{f}',
+                    python_callable=xml_to_csv,
+                    op_kwargs=dict(file=f'{AIRFLOW_HOME}/data/{SERVICE}_{f}.xml')
+                )
+                upload_data_to_gcs = PythonOperator(
+                    task_id=f'csv_to_gcs_{f}',
+                    python_callable=upload_to_gcs,
+                    op_kwargs=dict(bucket=BUCKET,
+                                   object_name=f'{SERVICE}/{f}.csv',
+                                   local_file=f'{AIRFLOW_HOME}/data/{SERVICE}_{f}.csv')
+                )
+                delete_bq_table = BigQueryDeleteTableOperator(
+                    task_id=f'delete_bq_table_{f}',
+                    deletion_dataset_table=f'{GCP_PROJECT}.{DATASET}.{SERVICE}_{f}',
+                    ignore_if_missing=True
+                )
+                create_bq_table = BigQueryCreateEmptyTableOperator(
+                    task_id=f'create_bq_table_{f}',
+                    dataset_id=DATASET,
+                    table_id=f'{SERVICE}_{f}',
+                    schema_fields=get_table_schema(f)['fields'],
+                    time_partitioning=get_table_schema(f).get('timePartitioning'),
+                    project_id=GCP_PROJECT,
+                    if_exists='log'
+                )
+                gcs_to_bq = GCSToBigQueryOperator(
+                    task_id=f'gcs_to_bq_{f}',
+                    bucket=BUCKET,
+                    source_objects=[f'{SERVICE}/{f}.csv'],
+                    destination_project_dataset_table=f"{DATASET}.{SERVICE}_{f}",
+                    write_disposition='WRITE_TRUNCATE',
+                    allow_quoted_newlines=True,
+                    max_bad_records=10
+                )
 
+                if prev_group:
+                    prev_group >> download_file
+                download_file >> convert_xml_to_csv >> upload_data_to_gcs >> delete_bq_table >> create_bq_table >> gcs_to_bq
+                prev_group = tg
 
-    # bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-    #     task_id='bigquery_external_table_task',
-    #     table_resource={
-    #         'tableReference': {
-    #             'projectId': 'dtc-de-course-412710',
-    #             'datasetId': 'datascience_stackexchange',
-    #             "tableId": 'tags'
-    #         },
-    #         'externalDataConfiguration': {
-    #             'sourceFormat': 'CSV',
-    #             'sourceUris': [f'gs://{BUCKET}/datascience/tags.csv']
-    #         },
-    #     },
-    #
-    # )
-    # bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-    #     task_id=f"create_external_table",
-    #     bucket=BUCKET,
-    #     source_objects=["/datascience/tags.csv"], #pass a list
-    #     destination_project_dataset_table=f"dtc-de-course-412710.datascience_stackexchange.tags",
-    #     source_format='CSV', #use source_format instead of file_format
-    # )
-    # "Count", "ExcerptPostId", 'WikiPostId'
-    # create_external_table = BigQueryCreateExternalTableOperator(
-    #     task_id="create_external_table",
-    #     destination_project_dataset_table=f"datascience_stackexchange.tags",
-    #     bucket=BUCKET,
-    #     source_objects=["datascience/tags.csv"],
-    #     source_format='CSV',
-        # schema_fields=[
-        #     {"name": "id", "type": "INTEGER", "mode": "REQUIRED"},
-        #     {"name": "tag_name", "type": "STRING", "mode": "NULLABLE"},
-        #     {'name': 'count', 'type': 'INTEGER', 'mode': 'NULLABLE'},
-        #     {'name': 'excerpt_post_id', 'type': 'INTEGER', 'mode': 'NULLABLE'},
-        #     {'name': 'wiki_post_id', 'type': 'INTEGER', 'mode': 'NULLABLE'}
-        # ],
-        # schema_fields = [
-        #     {'name': 'id', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {'name': 'post_type_id', 'type': 'INTEGER', 'mode': 'NULLABLE'},
-        #     {'name': 'creation_date', 'type': 'DATETIME', 'mode': 'NULLABLE'},
-        #     {'name': 'score', 'type': 'INTEGER', 'mode': 'NULLABLE'},
-        #     {'name': 'view_count', 'type': 'INTEGER', 'mode': 'NULLABLE'},
-        #     {'name': 'body', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {'name': 'owner_user_id', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {"name": "last_activity_date", "type": "DATETIME", "mode": "NULLABLE"},
-        #     {'name': 'title', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {'name': 'tags', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {"name": "answer_count", "type": "INTEGER", "mode": "NULLABLE"},
-        #     {'name': 'comment_count', 'type': 'INTEGER', 'mode': 'NULLABLE'},
-        #     {'name': 'closed_date', 'type': 'DATETIME', 'mode': 'NULLABLE'},
-        #     {"name": "content_license", "type": "STRING", "mode": "NULLABLE"},
-        #     {"name": "accepted_answer_id", "type": "INTEGER", "mode": "NULLABLE"},
-        #     {'name': 'last_editor_user_id', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {"name": "last_edit_date", "type": "DATETIME", "mode": "NULLABLE"},
-        #     {"name": "parent_id", "type": "INTEGER", "mode": "NULLABLE"},
-        #     {'name': 'owner_display_name', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {"name": "community_owned_date", "type": "DATETIME", "mode": "NULLABLE"},
-        #     {'name': 'last_editor_display_name', 'type': 'STRING', 'mode': 'NULLABLE'},
-        #     {'name': 'favourite_count', 'type': 'INTEGER', 'mode': 'NULLABLE'}
-        # ],
-        # skip_leading_rows=1
-    # )
-    # create_dataset_task = BigQueryCreateEmptyDatasetOperator(
-    #     task_id="create_dataset",
-    #     dataset_id="test_dataset",
-    #     location="eu",
-    # )
-    # test_conn = PythonOperator(
-    #     task_id=f'test_conn',
-    #     python_callable=test_conn
-    # )
-
-    # download_file >> convert_xml_to_csv >> upload_data_to_gcs >> load_csv
+        delete_local_files = BashOperator(
+            task_id='delete_local_files',
+            bash_command=f"rm -f {AIRFLOW_HOME}/data/*",
+            dag=dag
+        )
+        prev_group >> delete_local_files
